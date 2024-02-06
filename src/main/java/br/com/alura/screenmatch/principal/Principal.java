987145d67190fc7f432c7c9dca151e4a2923736e
AlexPortoNascimento <alexandreporto@id.uff.br>
2024-02-06 16:45:37 -0300

    Aula 4 - Explorando mais os fluxos e gerando estatísticas

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -4,11 +4,15 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
-import org.springframework.format.annotation.DateTimeFormat;
+//import org.springframework.format.annotation.DateTimeFormat;
 
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosSerie;
@@ -27,6 +31,8 @@ public class Principal {
     private final String API_KEY = "&apikey=43b972a";
     
     public void exibeMenu(){
+        
+        //Criando o buscador na API OMDB
         System.out.println("Digite o nome da série que desejas buscar");
         var nomeSerie = leitura.nextLine();
 		var json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
@@ -59,6 +65,7 @@ public class Principal {
             .flatMap(t -> t.episodios().stream())
             .collect(Collectors.toList());
 
+        //Criando um filtro de top5 episódios de acordo com a nota do IMDB    
         System.out.println("\nTop 5 episódios:");
         dadosEpisodios.stream()
             .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
@@ -73,6 +80,22 @@ public class Principal {
         
         episodio.forEach(System.out::println);
 
+        //Criando um buscador que diz qual temporada o episódio buscado pertence
+        System.out.println("Digite um trecho do nome do episódio que está buscando");
+        var trechoTitulo = leitura.nextLine();
+        
+        Optional<Episodio> episodioBuscado = episodio.stream()
+            .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+            .findFirst();
+
+        if(episodioBuscado.isPresent()) {
+            System.out.println("Episódio encontrado!");
+            System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
+        }else {
+            System.out.println("Episódio não encontrado.");
+        }
+
+        //Criando um filtro para ver episódios a partir da data desejada pelo usuário
         System.out.println("A partir de qual ano você deseja ver os episódios?");
         var ano = leitura.nextInt();
         leitura.nextLine();
@@ -87,8 +110,25 @@ public class Principal {
                 "Temporada: " + e.getTemporada() + 
                     " Episódio: " + e.getTitulo() +
                     " Data de Lançamento: " + e.getDatalancamento().format(formatador)
-            )); 
-                
+            ));
+
+        
+        //Criando um mapa com informações das avaliações por temporada
+        Map<Integer, Double> avaliacoesPorTemporada = episodio.stream()
+            .filter(e -> e.getAvaliacao() > 0.0)
+            .collect(Collectors.groupingBy(Episodio::getTemporada,
+                Collectors.averagingDouble(Episodio::getAvaliacao)));
+            
+        System.out.println(avaliacoesPorTemporada);
+
+        DoubleSummaryStatistics est = episodio.stream()
+            .filter(e -> e.getAvaliacao() > 0.0)
+            .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior Episódio: " + est.getMin());
+        System.out.println("Total de episódios avaliados: " + est. getCount());
     }
 
 }
